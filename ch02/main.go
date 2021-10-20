0810cb81c5b7161c8ac6a375e7cca7ee2be2b959
liuning108 <122273014@qq.com>
2021-10-20 10:11:31 +0800

    add recipe post for add

--- ch02/main.go
+++ ch02/main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
 	"time"
 )
 
 type Recipe struct {
-	Name        string    `json:"name"`
-	Tags        []string  `json:"tags"`
-	Ingredients []string  `json:"ingredients"`
-	PublishedAt time.Time `json:"publishedAt"`
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+
 }
 
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	err := router.Run()
 	if err != nil {
 		return
